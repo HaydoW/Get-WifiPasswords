05db26ce56f0f13114448a9a0200b9fdf9cd5346
HaydoW <46656949+HaydoW@users.noreply.github.com>
2020-07-30 12:09:23 +0100

    Add files via upload

---
+++ Get-WifiPasswords/Get-WifiPasswords/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+namespace Get_WifiPasswords
+{
+    class Program : TokenImpersonation
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine(@"Usage: .\Get-WifiPasswords.exe PID");
+                Environment.Exit(-1);
+            }
+
+            int PID;
+
+            if (int.TryParse(args[0], out PID))
+            {
+                GetSystemToken(PID);
+            }
+            else
+            {
+                Console.WriteLine(@"Usage: .\Get-WifiPasswords.exe PID");
+                Environment.Exit(-1);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+        public static void GetProfiles()
+        {
+            List<string> wifiProfiles = new List<string>();
+
+            string interfacePath = Path.GetPathRoot(Environment.SystemDirectory) + @"ProgramData\Microsoft\Wlansvc\Profiles\Interfaces\";
+
+            string[] adapterGUIDs = Directory.GetDirectories(interfacePath);
+
+            foreach (var guid in adapterGUIDs)
+            {
+                string[] arr = Directory.GetFiles(guid);
+                foreach (string i in arr)
+                {
+                    wifiProfiles.Add(i);
+                }
+            }
+
+            foreach (string wifiProfile in wifiProfiles)
+            {
+                GetContents(wifiProfile);
+            }
+        }
+
+        public static void GetContents(string wifiProfile)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(wifiProfile);
+            XmlNodeList ssid = xml.GetElementsByTagName("name");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n[+] SSID: {0}", ssid[0].InnerText);
+
+            XmlNodeList keyMaterial = xml.GetElementsByTagName("keyMaterial");
+            if (keyMaterial[0].InnerText.Equals(""))
+            {
+                Console.WriteLine("[!] Password: Not found!");
+            }
+            else
+            {
+                Console.WriteLine("[+] Password: {0}\n", Decrypt(keyMaterial[0].InnerText));
+            }
+
+        }
+
+        public static string Decrypt(string encPassword)
+        {
+            byte[] passwordBytes = new byte[encPassword.Length / 2];
+
+            for (int i = 0; i < encPassword.Length; i += 2)
+                passwordBytes[i / 2] = Convert.ToByte(encPassword.Substring(i, 2), 16);
+
+            byte[] unprotectedBytes = ProtectedData.Unprotect(passwordBytes, null, DataProtectionScope.LocalMachine);
+
+            return Encoding.ASCII.GetString(unprotectedBytes);
+        }
+
+        public static void GetSystemToken(int pid)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n[+] Attempting to impersonate token of PID: {0}", pid);
+
+            if (!ImpersonateProcessToken(pid))
+            {
+                Console.WriteLine("[!] Could not impersonate token! Exiting...");
+                Environment.Exit(-1);
+            }
+            else
+            {
+                Console.WriteLine("[+] Attempting to enable all privileges...");
+                if (!EnablePrivilege("SeIncreaseQuotaPrivilege"))
+                {
+                    Console.WriteLine("[!] Could not enable all privileges! Exiting...");
+                    Environment.Exit(-1);
+                }
+                else
+                {
+                    Console.WriteLine("[+] Attempting to dump wifi credentials...");
+                    GetProfiles();
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Get-WifiPasswords/Get-WifiPasswords/TokenImpersonation.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Get_WifiPasswords
+{
+
+    //Token Impersonation credit - https://0x00-0x00.github.io/research/2018/10/21/Windows-API-And-Impersonation-Part-2.html
+
+    public class TokenImpersonation
+    {
+        // Constants that are going to be used during our procedure.
+        private const int ANYSIZE_ARRAY = 1;
+        public static uint SE_PRIVILEGE_ENABLED = 0x00000002;
+        public static uint STANDARD_RIGHTS_REQUIRED = 0x000F0000;
+        public static uint STANDARD_RIGHTS_READ = 0x00020000;
+        public static uint TOKEN_ASSIGN_PRIMARY = 0x00000001;
+        public static uint TOKEN_DUPLICATE = 0x00000002;
+        public static uint TOKEN_IMPERSONATE = 0x00000004;
+        public static uint TOKEN_QUERY = 0x00000008;
+        public static uint TOKEN_QUERY_SOURCE = 0x00000010;
+        public static uint TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+        public static uint TOKEN_ADJUST_GROUPS = 0x00000040;
+        public static uint TOKEN_ADJUST_DEFAULT = 0x00000080;
+        public static uint TOKEN_ADJUST_SESSIONID = 0x00000100;
+        public static uint TOKEN_READ = STANDARD_RIGHTS_READ | TOKEN_QUERY;
+        public static uint TOKEN_ALL_ACCESS = STANDARD_RIGHTS_REQUIRED | TOKEN_ASSIGN_PRIMARY | TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_QUERY_SOURCE | TOKEN_ADJUST_PRIVILEGES | TOKEN_ADJUST_GROUPS | TOKEN_ADJUST_DEFAULT | TOKEN_ADJUST_SESSIONID;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID_AND_ATTRIBUTES
+        {
+            public LUID Luid;
+            public UInt32 Attributes;
+
+            public const UInt32 SE_PRIVILEGE_ENABLED_BY_DEFAULT = 0x00000001;
+            public const UInt32 SE_PRIVILEGE_ENABLED = 0x00000002;
+            public const UInt32 SE_PRIVILEGE_REMOVED = 0x00000004;
+            public const UInt32 SE_PRIVILEGE_USED_FOR_ACCESS = 0x80000000;
+        }
+
+        // Luid Structure Definition
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID
+        {
+            public UInt32 LowPart;
+            public Int32 HighPart;
+        }
+
+        public struct TOKEN_PRIVILEGES
+        {
+            public int PrivilegeCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = ANYSIZE_ARRAY)]
+            public LUID_AND_ATTRIBUTES[] Privileges;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRIVILEGE_SET
+        {
+            public uint PrivilegeCount;
+            public uint Control;  // use PRIVILEGE_SET_ALL_NECESSARY
+
+            public static uint PRIVILEGE_SET_ALL_NECESSARY = 1;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+            public LUID_AND_ATTRIBUTES[] Privilege;
+        }
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        // LookupPrivilegeValue
+        [DllImport("advapi32.dll")]
+        static extern bool LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
+
+        // OpenProcess
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(
+         ProcessAccessFlags processAccess,
+         bool bInheritHandle,
+         int processId);
+        public static IntPtr OpenProcess(Process proc, ProcessAccessFlags flags)
+        {
+            return OpenProcess(flags, false, proc.Id);
+        }
+
+        // OpenProcessToken
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool OpenProcessToken(IntPtr ProcessHandle, UInt32 DesiredAccess, out IntPtr TokenHandle);
+
+        // DuplicateToken
+        [DllImport("advapi32.dll")]
+        public extern static bool DuplicateToken(IntPtr ExistingTokenHandle, int SECURITY_IMPERSONATION_LEVEL, ref IntPtr DuplicateTokenHandle);
+
+        // SetThreadToken
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetThreadToken(IntPtr pHandle, IntPtr hToken);
+
+        // AdjustTokenPrivileges
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AdjustTokenPrivileges(IntPtr TokenHandle,
+           [MarshalAs(UnmanagedType.Bool)]bool DisableAllPrivileges,
+           ref TOKEN_PRIVILEGES NewState,
+           UInt32 BufferLengthInBytes,
+           ref TOKEN_PRIVILEGES PreviousState,
+           out UInt32 ReturnLengthInBytes);
+
+        // GetCurrentProcess
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetCurrentProcess();
+
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool PrivilegeCheck(
+            IntPtr ClientToken,
+            ref PRIVILEGE_SET RequiredPrivileges,
+            out bool pfResult
+            );
+
+        // Now I will create functions that use the above definitions, so we can use them directly from PowerShell :P
+        public static bool IsPrivilegeEnabled(string Privilege)
+        {
+            bool ret;
+            LUID luid = new LUID();
+            IntPtr hProcess = GetCurrentProcess();
+            IntPtr hToken;
+            if (hProcess == IntPtr.Zero) return false;
+            if (!OpenProcessToken(hProcess, TOKEN_QUERY, out hToken)) return false;
+            if (!LookupPrivilegeValue(null, Privilege, out luid)) return false;
+            PRIVILEGE_SET privs = new PRIVILEGE_SET { Privilege = new LUID_AND_ATTRIBUTES[1], Control = PRIVILEGE_SET.PRIVILEGE_SET_ALL_NECESSARY, PrivilegeCount = 1 };
+            privs.Privilege[0].Luid = luid;
+            privs.Privilege[0].Attributes = LUID_AND_ATTRIBUTES.SE_PRIVILEGE_ENABLED;
+            if (!PrivilegeCheck(hToken, ref privs, out ret)) return false;
+            return ret;
+        }
+
+        public static bool EnablePrivilege(string Privilege)
+        {
+            LUID luid = new LUID();
+            IntPtr hProcess = GetCurrentProcess();
+            IntPtr hToken;
+            if (!OpenProcessToken(hProcess, TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES, out hToken)) return false;
+            if (!LookupPrivilegeValue(null, Privilege, out luid)) return false;
+            // First, a LUID_AND_ATTRIBUTES structure that points to Enable a privilege.
+            LUID_AND_ATTRIBUTES luAttr = new LUID_AND_ATTRIBUTES { Luid = luid, Attributes = LUID_AND_ATTRIBUTES.SE_PRIVILEGE_ENABLED };
+            // Now we create a TOKEN_PRIVILEGES structure with our modifications
+            TOKEN_PRIVILEGES tp = new TOKEN_PRIVILEGES { PrivilegeCount = 1, Privileges = new LUID_AND_ATTRIBUTES[1] };
+            tp.Privileges[0] = luAttr;
+            TOKEN_PRIVILEGES oldState = new TOKEN_PRIVILEGES(); // Our old state.
+            if (!AdjustTokenPrivileges(hToken, false, ref tp, (UInt32)Marshal.SizeOf(tp), ref oldState, out UInt32 returnLength)) return false;
+            return true;
+        }
+
+        public static bool ImpersonateProcessToken(int pid)
+        {
+            IntPtr hProcess = OpenProcess(ProcessAccessFlags.QueryInformation, true, pid);
+            if (hProcess == IntPtr.Zero) return false;
+            IntPtr hToken;
+            if (!OpenProcessToken(hProcess, TOKEN_IMPERSONATE | TOKEN_DUPLICATE, out hToken)) return false;
+            IntPtr DuplicatedToken = new IntPtr();
+            if (!DuplicateToken(hToken, 2, ref DuplicatedToken)) return false;
+            if (!SetThreadToken(IntPtr.Zero, DuplicatedToken)) return false;
+            return true;
+        }
+    }
+}
